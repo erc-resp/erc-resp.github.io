5651fb4edc23c5f223f8f516b8083bce7f1335fb
erc-resp <ercresp@gmail.com>
2021-10-11 10:35:25 +0200

    language

---
+++ italian/js/owlscript.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+$('.owl-carousel').owlCarousel({
+  loop:true,
+  margin:0,
+  nav:true,
+  center: true,
+  autoplay:true,
+  autoplayhoverpause:true,
+  autoplaytimeout:50,
+  items:3,
+  nav: true,
+  loop: true,
+  responsive:{
+    0:{
+        items: 1
+    },
+    600:{
+        items: 2
+    },
+    1000:{
+        items: 3
+    }
+}
+
+});z
+});
